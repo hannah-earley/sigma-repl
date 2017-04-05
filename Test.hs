b921661bc501f4459301dc28c65fd7a219e185e2
Hannah Earley <h@nnah.io>
2017-04-05 03:52:59 +0100

    rewrote scoping yet again, now its implementation is much simpler and (hopefully) more robust too

--- Test.hs
+++ Test.hs
@@ -1,35 +1,41 @@
-import Scope
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+import Scope2
 import Data.List (intercalate)
+import Data.Maybe (fromJust)
 import qualified Data.Map.Lazy as Map
 
 --- scopes
 
-instance Show r => Show (Context r d) where
-  show = intercalate " > " . reverse . ctxTrail
+instance {-# OVERLAPS #-} (Show r, Show d) => Show (Context r d) where
+  show (s, cs, r, d) = "<" ++ e ++ " | " ++ c ++ " | "
+                           ++ show r ++ " -> " ++ show d ++ ">\n"
+    where
+      e = show $ exposed s
+      c = intercalate ":" (cs ++ [show r])
 
-dl :: Ord r => [(r,d)] -> Scopy r d
-dl = MkScopy . DefList . Map.fromListWith (++) . map (pure <$>)
+dl = defines . Map.fromListWith error
 
-ws = dl [(1,"a"),(2,"b"),(3,"c"),(4,"e")]
+ws = dl [(1,"a"),(2,"b"),(3,"c"),(41,"e")]
 xs = dl [(4,"d"),(5,"e"),(6,"f")]
 ys = dl [(7,"g"),(8,"h"),(9,"i")]
-zs = dl [(1,"j"),(1,"jj"),(2,"k"),(3,"l")]
+zs = dl [(21,"j"),(22,"k"),(23,"l")]
 
-g = ScopeGroup [ws,zs]
-h = ScopeGroup [xs,ys]
-i = ScopeGroup $ [MkScopy h, ws, j, MkScopy p, MkScopy q]
+g = fromJust $ group [ws,zs]
+h = fromJust $ group [xs,ys]
+i = fromJust $ group [h, ws, p, j, protect zs]
 
-private :: (Scope s, Ord r) => s r d -> [r] -> Restricted r d
-private s rs = Restricted s (Map.fromList $ zip rs rs)
+--private :: Ord r => Scope r d -> [r] -> Scope r d
+private s rs = restrict s (Map.fromList $ zip rs rs)
 
-p = private (dl [(1,"p"),(1,"pp"),(10,"q"), (11, "r"), (12,"z")]) [1,11]
-q = Restricted (dl [(1,"m"),(2,"nn"),(2,"n"),(3,"o")]) $
-          Map.fromList [(13,1),(14,2),(7,3)]
+p = private (dl [(1,"p"),(10,"q"), (11, "r"), (12,"z")]) [10,11]
+q = restrict (dl [(1,"m"),(2,"n"),(3,"o")])
+          $ Map.fromList [(13,1),(14,2),(17,3)]
 
-j = MkScopy . Insulated $ ScopeGroup [zs,zs]
-k = MkScopy $ mkFile "k.sig" (Map.fromList $ zip [0..6] [1..7]) i
+j = protect q
+k = file "k.sig" (Map.fromList $ zip [0..6] [1..7]) i
 
-go :: Ord r => Int -> [(d, Context r d)] -> r -> [(d, Context r d)]
-go n = get . snd . (!!n)
-
----
+go :: Ord r => Maybe (Context r d) -> r -> Maybe (Context r d)
+go (Just (s, _, _, _)) = resolve s
+go Nothing = const Nothing
