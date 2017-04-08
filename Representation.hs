eecfe1fc2e6c2d9e7b2f0538cb5f837e9063f6e0
William Earley <w.earley@damtp.cam.ac.uk>
2017-04-08 17:46:45 +0100

    new simplified model structure

--- Representation.hs
+++ Representation.hs
@@ -6,10 +6,11 @@ module Representation
 ( module Representation
 ) where
 
-import Common (Partition, repartition, index, pmap, converge)
+import Common (Partition, subpartition, index, pmap, converge)
+import qualified Data.Map.Lazy as M
 
 distinguish :: Ord r => Partition (r, [r]) -> Partition (r, [r])
-distinguish = repartition =<< (. snd) . map . index . pmap fst
+distinguish = subpartition =<< (. snd) . map . (M.!) . index . pmap fst
 
 dedup :: Ord r => Partition (r, [r]) -> Partition r
 dedup = pmap fst . converge (==) distinguish
