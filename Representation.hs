891fb06820950962901f47137581a97cac9ce165
Hannah Earley <h@nnah.io>
2017-04-10 18:11:51 +0100

    added disclaimer for graph isomorphism algorithm

--- Representation.hs
+++ Representation.hs
@@ -21,6 +21,21 @@ dedup = pmap fst . converge (==) distinguish
 duprep :: (Ord r, Ord s) => [(r, (s, [r]))] -> Partition (r, [r])
 duprep = pmap (fmap snd) . partition (fst . snd) . S.fromList
 
+-- in theory, this creates a unique representation of the deäliased subgraphs
+-- rooted by each ref in xs; the representation is a list of the unique nodes
+-- in the subgraph, with the root first, and each node represented by its
+-- signature and the nodes it points to (indexed by their position in the
+-- list). The positions should correspond to a breadth-first search, and
+-- also inherit order from the reference order. Thus it should provide an
+-- efficient test for labelled directed cyclic graph isomorphism....
+--
+-- graph isomorphism is generally a hard problem though, not known if p or np,
+-- so this algorithm may be incorrect; hopefully it is correct as each node's
+-- outedges have a unique set of labels
+--
+-- by doi:10.1007/11751649_46, this should be fine? unique labels
+-- should reduce to O(n) for n nodes, and so our special case algorithm
+-- should work well....
 uniqify :: (Ord r, Ord s) => [(r, (s, [r]))] -> [(r, [(s, [Int])])]
 uniqify xs = map (ap (,) bfs . fst) xs
   where
