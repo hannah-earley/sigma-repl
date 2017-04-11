c9559510d0403f4c566139bd17cbe18ff547e48e
William Earley <w.earley@damtp.cam.ac.uk>
2017-04-11 17:22:04 +0100

    started to implement parsing

--- Input.hs
+++ Input.hs
@@ -2,4 +2,42 @@ module Input
 ( module Input
 ) where
 
-import Text.Parsec ()
+import qualified Text.Parsec as P
+import qualified Text.Parsec.Token as T
+import Data.Char (isSpace, isDigit)
+import Data.Functor.Identity (Identity)
+-- import qualified Model as M
+
+--- parsec bootstrapping
+
+lexer :: T.GenTokenParser String () Identity
+lexer = T.makeTokenParser T.LanguageDef
+      { T.commentStart = "(*"
+      , T.commentEnd = "*)"
+      , T.commentLine = ";"
+      , T.nestedComments = True
+      , T.identStart = P.satisfy $ not . reservedIdStart
+      , T.identLetter = P.satisfy $ not . reservedIdLetter
+      , T.reservedNames = [ "inherit", "inherit*", "inh", "inh*"
+                          , "bequeath", "bequeath*", "beq", "beq*"
+                          , "def", "def*", "perm", "perm*"
+                          , "group", "grp" ]
+      , T.caseSensitive = True
+      , T.opStart = undefined
+      , T.opLetter = undefined
+      , T.reservedOpNames = undefined }
+  where
+    reservedIdLetter c = isSpace c || c `elem` ".#@<>(){}[]`':\""
+    reservedIdStart c = reservedIdLetter c || isDigit c
+
+symbol = T.symbol lexer
+identifier = T.identifier lexer
+reserved = T.reserved lexer
+litStr = T.stringLiteral lexer
+litNat = T.natural lexer
+parens = T.parens lexer
+braces = T.braces lexer
+angles = T.angles lexer
+brackets = T.brackets lexer
+colon = T.colon lexer
+dot = T.dot lexer
