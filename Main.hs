6907868427b13344b1e4dbce260152a94e536edb
Hannah Earley <h@nnah.io>
2017-05-05 16:55:47 +0100

    basic repl

--- Main.hs
+++ Main.hs
@@ -1,4 +1,21 @@
 module Main where
 
+import System.Environment (getArgs)
+import System.Console.Haskeline
+  ( defaultSettings, withInterrupt, getInputLine
+  , outputStrLn, InputT, runInputT, handleInterrupt )
+import Control.Monad (foldM)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = getArgs >>= foldM load emptyContext
+               >>= runInputT defaultSettings . withInterrupt . loop
+  where
+    emptyContext = ()
+    load _ = putStrLn . ("Loaded " ++)
+
+loop :: () -> InputT IO ()
+loop c = handleInterrupt (loop c) $
+         do cmd <- getInputLine "σ> "
+            case cmd of
+              Just cmd' -> outputStrLn (cmd' ++ "\n") >> loop c
+              Nothing -> return ()
