dd7cb02f82e0fadd3d5bf53f391edea00c1bdaff
William Earley <w.earley@damtp.cam.ac.uk>
2017-05-21 21:48:09 +0100

    conversion functions from SigmaTokens to Sigmas and Perms

--- src/Common.hs
+++ src/Common.hs
@@ -4,22 +4,22 @@ module Common
 
 import Data.Typeable (Typeable)
 import qualified Control.Exception as E
-import Data.List (intercalate)
 
 type ID = String
 
 data ReadError = LocateError FilePath
                | ParseError String
                | IncompleteError String
-               | ImportError FilePath [ID]
+               | ReferenceError ID
                | InconsistencyError FilePath
+               | OtherError String
                deriving (Show, Typeable)
 
 instance E.Exception ReadError where
   displayException (LocateError p) = "Couldn't locate " ++ p
   displayException (ParseError e) = e
   displayException (IncompleteError e) = e
-  displayException (ImportError p xs) =
-    "Couldn't import " ++ intercalate ", " xs ++ " from " ++ p
+  displayException (ReferenceError x) = "Couldn't dereference " ++ x
   displayException (InconsistencyError p) =
     "File " ++ p ++ " changed whilst loading"
+  displayException (OtherError e) = e
--- src/Graph.hs
+++ src/Graph.hs
@@ -77,31 +77,34 @@ addEdge g f e = g {nodes = M.adjust (fmap (e:)) f $ nodes g}
 addEdges :: Graph -> Int -> [Edge] -> Graph
 addEdges g f = foldr (\e g' -> addEdge g' f e) g
 
----
+--- location
+
+overroot :: Graph -> Graph
+overroot g = g { root = overture g }
 
 reroot :: Graph -> Int -> Graph
 reroot g r = g { root = r }
 
--- discover :: Graph -> [(ID, Int)]
--- discover g = discover' (nodes g) (root g)
---
--- discover' :: M.Map Int (Node, [Edge]) -> Int -> [(ID, Int)]
--- discover' m n = undefined
+search :: Graph -> ID -> [(Int, P.SigmaToken)]
+search g r = searchAt g (root g) r
 
-search :: Graph -> ID -> [Int]
-search g r = reverse . snd $ go (S.empty, []) (root g, r)
+searchAt :: Graph -> Int -> ID -> [(Int, P.SigmaToken)]
+searchAt g n r = reverse . snd $ go (S.empty, []) (n, r)
   where
-    go h@(s,l) q@(n,_)
+    go h@(s,l) q@(n',_)
       | S.member q s = h
-      | otherwise = go' (S.insert q s) l q . M.lookup n $ nodes g
+      | otherwise = go' (S.insert q s) l q . M.lookup n' $ nodes g
 
     go' s l _ Nothing = (s, l)
-    go' s l (n,r') (Just (x,es)) =
+    go' s l (n',r') (Just (x,es)) =
       let queue = catMaybes . map (query r') $ sort es
-      in foldl go (s, collect n x ++ l) queue
+      in foldl go (s, collect n' x ++ l) queue
 
-    collect n (Def _ _) = [n]
+    collect n' (Def _ d) = [(n',d)]
     collect _ _ = []
 
-    query r' (Edge (Qualified p) _ n) = (n,) <$> stripPrefix p r'
-    query r' (Edge (Single x y) _ n) = guard (x == r') >> return (n,y)
+    query r' (Edge (Qualified p) _ n') = (n',) <$> stripPrefix p r'
+    query r' (Edge (Single x y) _ n') = guard (x == r') >> return (n',y)
+
+get :: Graph -> Int -> Maybe P.SigmaToken
+get g n = do { (Def _ s, _) <- M.lookup n $ nodes g ; return s }
